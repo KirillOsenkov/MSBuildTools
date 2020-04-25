06103ba9fef6bab0204362406c2e1be1a4944ce7
Kirill Osenkov <github@osenkov.com>
2020-04-24 20:02:18 -0700

    Add a new project for ToolsetLayout.

---
+++ src/ToolsetLayout/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToolsetLayout
+{
+    public class Entrypoint
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
