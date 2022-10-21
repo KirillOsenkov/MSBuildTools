f9128ef50f49f2a18257ce807d749682e3b09915
Kirill Osenkov <github@osenkov.com>
2022-10-21 09:31:47 -0700

    Add repro for 8075

---
+++ repro/8075/Task/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Build.Utilities;
+
+namespace Task
+{
+    public class Class1 : ToolTask
+    {
+        protected override string ToolName => "SpecialSauce";
+
+        protected override string GenerateFullPathToTool()
+        {
+            return @"C:\temp\msbuild_8075\silly\bin\Debug\net6.0\silly.exe";
+        }
+
+        public override bool Execute()
+        {
+            base.Execute();
+            return base.Execute();
+        }
+    }
+}
