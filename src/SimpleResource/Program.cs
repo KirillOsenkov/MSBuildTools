d8afc187fa7be3771743e223a31765a68248f4e5
Kirill Osenkov <kirillo@microsoft.com>
2023-08-22 15:16:32 +0530

    Add a sample of embedding a file as a resource

---
+++ src/SimpleResource/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var assembly = typeof(Program).Assembly;
+        var stream = assembly.GetManifestResourceStream(@"embedded.txt");
+
+        var reader = new StreamReader(stream);
+        var text = reader.ReadToEnd();
+
+        Console.WriteLine(text);
+    }
+}
