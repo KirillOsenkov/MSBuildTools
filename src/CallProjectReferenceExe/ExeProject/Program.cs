155f652e622575269358727ee3f8bfb1ab226d89
Kirill Osenkov <github@osenkov.com>
2024-01-18 16:59:55 -0800

    Add a sample of running an .exe produced by a referenced project

---
+++ src/CallProjectReferenceExe/ExeProject/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        foreach (var arg in args)
+        {
+            Console.WriteLine(arg);
+        }
+    }
+}
