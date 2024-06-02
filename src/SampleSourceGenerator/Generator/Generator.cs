b35507f1c04767aeaebafc6cdc4623b4ae655599
Kirill Osenkov <github@osenkov.com>
2024-06-01 17:31:13 -0700

    Add

---
+++ src/SampleSourceGenerator/Generator/Generator.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis;
+
+namespace Generator
+{
+    [Generator]
+    public class SourceGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            string text =
+                """
+                namespace GeneratedNamespace
+                {
+                    public partial class GeneratedClass
+                    {
+                        public static void GeneratedMethod()
+                        {
+                           System.Console.WriteLine("hello world");
+                        }
+                    }
+                }
+                """;
+
+            context.RegisterPostInitializationOutput(ctx => ctx.AddSource(
+                "myGeneratedFile.g.cs",
+                SourceText.From(text, Encoding.UTF8)));
+        }
+    }
+}
